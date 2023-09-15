fe5967e8634a64140a935ca5b42d622cd435711c
Miryam Yanit Baranda Moreno <miryamybm@gmail.com>
2023-09-15 12:45:29 -0600

    response in JSON format

--- index.js
+++ index.js
@@ -5,24 +5,26 @@ const app= express()
 
 //los callback pueden recibir tres parametros= req(recibe parametros de la URL) y res(funcion que permite dar respuesta)
 app.get('/',(request, response)=>{
-    response.send("Hello GET, i'm Miryam")
+    //response.send("Hello GET, i'm Miryam")
+    //respuesta en formato JSON
+    response.json({msg: "Hello GET, I'm Miryam"})
 })
-/*los navegadores solo soportan GET para utilizar estos metodos se necesita un emulador
+//los navegadores solo soportan GET para utilizar estos metodos se necesita un emulador
 app.post('/',(request, response)=>{
-    response.send("Hello POST, i'm Miryam")
+    response.json({msg: "Hello POST, I'm Miryam"})
 })
 
 app.put('/',(request, response)=>{
-    response.send("Hello PUT, i'm Miryam")
+    response.json({msg: "Hello PUT, I'm Miryam"})
 })
 
 app.patch('/',(request, response)=>{
-    response.send("Hello PATCH, i'm Miryam")
+    response.json({msg: "Hello PATCH, I'm Miryam"})
 })
 
 app.delete('/',(request, response)=>{
-    response.send("Hello DELETE, i'm Miryam")
-})*/
+    response.json({msg: "Hello DELETE, I'm Miryam"})
+})
 
 /**
  * FUNCIONALIDAD 
@@ -30,6 +32,7 @@ app.delete('/',(request, response)=>{
  */
 //se coloca un callback aunque no es necesario 
 //http;//localhost:3000
+
 app.listen(3000,() => {
     console.log('Listening on port 3000')
 })
